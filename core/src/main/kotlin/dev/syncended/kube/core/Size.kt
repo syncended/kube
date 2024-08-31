e0c4a9104ef5eaf6dd5c327d16f4bbb132dc615e
syncended <syncended@gmail.com>
2024-08-31 22:10:08 +0300

    add stylings

--- core/src/main/kotlin/dev/syncended/kube/core/Size.kt
+++ core/src/main/kotlin/dev/syncended/kube/core/Size.kt
@@ -1,6 +1,23 @@
 package dev.syncended.kube.core
 
-sealed class Size {
-  data class Px(val value: Int): Size()
-  data class Percent(val value: Int): Size()
+import kotlinx.css.LinearDimension
+
+sealed class Size(private val units: String) {
+  abstract val value: Any
+
+  data class Px(override val value: Int) : Size("px")
+  data class Rem(override val value: Double) : Size("rem")
+  data class Em(override val value: Double) : Size("em")
+  data class Percent(override val value: Int) : Size("%")
+
+  internal fun Size.toDimension(): LinearDimension {
+    return LinearDimension(toRawValue())
+  }
+
+  private fun toRawValue(): String = "$value$units"
 }
+
+val Int.px: Size.Px get() = Size.Px(this)
+val Int.percent: Size.Percent get() = Size.Percent(this)
+val Double.rem: Size.Rem get() = Size.Rem(this)
+val Double.em: Size.Em get() = Size.Em(this)

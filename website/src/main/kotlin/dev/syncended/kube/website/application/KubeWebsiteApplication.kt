a0c4b44c956a21530d7c45fb63b63fae91803966
syncended <syncended@gmail.com>
2024-09-11 01:37:37 +0300

    remove configuration import

--- website/src/main/kotlin/dev/syncended/kube/website/application/KubeWebsiteApplication.kt
+++ website/src/main/kotlin/dev/syncended/kube/website/application/KubeWebsiteApplication.kt
@@ -1,11 +1,8 @@
 package dev.syncended.kube.website.application
 
-import dev.syncended.kube.spring.configuration.KubeConfiguration
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Import
 
-@Import(KubeConfiguration::class)
 @SpringBootApplication
 open class KubeWebsiteApplication {
   companion object {

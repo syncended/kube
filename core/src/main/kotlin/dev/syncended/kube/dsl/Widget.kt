8e690b5890779f510f7f5f966fa8a22c91cef9ff
syncended <syncended@gmail.com>
2024-09-20 00:57:32 +0300

    rewrite widgets

--- core/src/main/kotlin/dev/syncended/kube/dsl/Widget.kt
+++ core/src/main/kotlin/dev/syncended/kube/dsl/Widget.kt
@@ -1,45 +1,65 @@
 package dev.syncended.kube.dsl
 
 import dev.syncended.kube.components.Image
-import dev.syncended.kube.components.ImageModifier
 import dev.syncended.kube.components.ImageType
 import dev.syncended.kube.components.Space
-import dev.syncended.kube.components.alt
-import dev.syncended.kube.components.base64Image
-import dev.syncended.kube.components.base64Type
-import dev.syncended.kube.components.imageUrl
 import dev.syncended.kube.core.component.Layout
-import dev.syncended.kube.core.component.DeprModifier
+import dev.syncended.kube.core.component.Modifier
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
 
-fun Layout<*>.space(body: Space.() -> Unit) = widget(Space(DeprModifier()), body)
+fun Layout.space(modifier: Modifier = Modifier) = widget(Space(modifier = modifier))
 
-fun Layout<*>.image(url: String, alt: String? = null, body: Image.() -> Unit = {}) = widget(
-  instance = Image(ImageModifier().imageUrl(url).alt(alt)),
-  body = body
+fun Layout.image(
+  imageUrl: String,
+  modifier: Modifier = Modifier,
+  alt: String? = null
+) = image(
+  modifier = modifier,
+  imageUrl = imageUrl,
+  alt = alt,
+  base64Image = null,
+  base64Type = null
 )
 
-fun Layout<*>.image(
-  rawImage: ByteArray,
+fun Layout.image(
+  base64Image: String,
+  modifier: Modifier = Modifier,
   type: ImageType = ImageType.PNG,
   alt: String? = null,
-  body: Image.() -> Unit = {}
-) = widget(
-  instance = Image(
-    ImageModifier()
-      .base64Image(rawImage)
-      .base64Type(type)
-      .alt(alt)
-  ),
-  body = body
+) = image(
+  modifier = modifier,
+  imageUrl = null,
+  alt = alt,
+  base64Image = base64Image,
+  base64Type = type
 )
 
-fun Layout<*>.svgImage(
-  rawImage: ByteArray,
+fun Layout.image(
+  base64Image: ByteArray,
+  modifier: Modifier = Modifier,
+  type: ImageType = ImageType.PNG,
   alt: String? = null,
-  body: Image.() -> Unit = {}
-) = image(rawImage = rawImage, type = ImageType.SVG, alt = alt, body)
+) = image(
+  modifier = modifier,
+  imageUrl = null,
+  alt = alt,
+  base64Image = @OptIn(ExperimentalEncodingApi::class) Base64.encode(base64Image),
+  base64Type = type
+)
 
-fun Layout<*>.image(body: Image.() -> Unit = {}) = widget(
-  instance = Image(ImageModifier()),
-  body = body
+private fun Layout.image(
+  modifier: Modifier,
+  imageUrl: String?,
+  base64Image: String?,
+  base64Type: ImageType?,
+  alt: String?,
+) = widget(
+  instance = Image(
+    modifier = modifier,
+    imageUrl = imageUrl,
+    base64Image = base64Image,
+    base64Type = base64Type,
+    alt = alt
+  )
 )

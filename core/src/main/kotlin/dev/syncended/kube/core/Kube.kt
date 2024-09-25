65fe43b9260cba5d94f696800ac4d927d087a1f2
syncended <syncended@gmail.com>
2024-09-26 00:32:43 +0300

    kube-2: finish plugins system

--- core/src/main/kotlin/dev/syncended/kube/core/Kube.kt
+++ core/src/main/kotlin/dev/syncended/kube/core/Kube.kt
@@ -2,23 +2,16 @@ package dev.syncended.kube.core
 
 import dev.syncended.kube.core.component.Widget
 import dev.syncended.kube.core.model.RenderMode
-import dev.syncended.kube.core.model.ResourceMode
 import dev.syncended.kube.core.plugins.KubePlugins
-import dev.syncended.kube.htmx.htmxMinJs
 import kotlinx.html.HTML
 import kotlinx.html.body
 import kotlinx.html.dom.createHTMLDocument
 import kotlinx.html.dom.serialize
 import kotlinx.html.head
 import kotlinx.html.html
-import kotlinx.html.script
-import kotlinx.html.unsafe
-import trimSlashes
 
 object Kube {
   internal val plugins = PluginsHolder()
-
-  private var isHtmxEnabled = false
 
   init {
     install(KubePlugins)
@@ -32,10 +25,6 @@ object Kube {
     plugins.remove(plugin)
   }
 
-  fun setHtmxEnabled(isEnabled: Boolean) {
-    isHtmxEnabled = isEnabled
-  }
-
   internal fun render(mode: RenderMode, root: Widget): String {
     return createHTMLDocument().html {
       renderHead(mode)
@@ -47,12 +36,6 @@ object Kube {
     if (mode == RenderMode.VIEW_ONLY) return
     head {
       plugins.head.forEach { it.apply(this) }
-      when (plugins.resources.mode) {
-        ResourceMode.FAT -> if (isHtmxEnabled) script { unsafe { +htmxMinJs() } }
-        ResourceMode.LINK -> if (isHtmxEnabled) script {
-          src = "/${plugins.resources.prefix}/js/htmx.min.js".trimSlashes()
-        }
-      }
     }
   }
 }

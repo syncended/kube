4201b626d94dc4dd2113f4d3aab1d3e225a3001e
syncended <syncended@gmail.com>
2025-03-11 00:54:40 +0300

    playground colors

--- playground/src/main/kotlin/dev/syncended/kube/PlaygroundUi.kt
+++ playground/src/main/kotlin/dev/syncended/kube/PlaygroundUi.kt
@@ -2,15 +2,31 @@ package dev.syncended.kube
 
 import dev.syncended.kube.components.ui.TextInputType
 import dev.syncended.kube.core.component.Layout
+import dev.syncended.kube.core.component.Modifier
+import dev.syncended.kube.core.component.backgroundColor
+import dev.syncended.kube.core.component.fillMaxSize
+import dev.syncended.kube.core.model.Alignment
+import dev.syncended.kube.core.model.Color
+import dev.syncended.kube.core.model.toColor
+import dev.syncended.kube.dsl.box
 import dev.syncended.kube.dsl.column
 import dev.syncended.kube.dsl.form
 import dev.syncended.kube.dsl.hiddenInput
 import dev.syncended.kube.dsl.text
 import dev.syncended.kube.dsl.textInput
+import dev.syncended.kube.styling.Colors
 
-fun Layout.renderUi() = column {
-  text("Hello playground!")
-  renderForm()
+private val gray = "#666".toColor()
+
+fun Layout.renderUi() = box(
+  modifier = Modifier.backgroundColor(gray).fillMaxSize(),
+  verticalAlignment = Alignment.Vertical.Center,
+  horizontalAlignment = Alignment.Horizontal.Center
+) {
+  column {
+    text("Hello playground!")
+    renderForm()
+  }
 }
 
 fun Layout.renderForm() = form {
